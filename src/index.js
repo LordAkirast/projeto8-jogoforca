7113a42d9b542e89585ff7386ed09541d8c4cb96
LordAkirast <darkwarish@gmail.com>
2022-11-22 17:58:18 -0300

    Hangzone added. Wordzone added. Button added. Stylesheet created.

--- src/index.js
+++ src/index.js
@@ -4,5 +4,14 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div>aaa</div>
+  <div className="game_window">
+    <div className='hangwordzone'>
+      <div className='hangarea' title='hangarea'>
+        <img className="hangman" title='hangman' src="./assets/forca0.png" alt="1 out of 6" />
+      </div>
+      <div className='wordarea'>
+        <button className='button'>Escolher Palavra</button>
+      </div>
+    </div>
+  </div>
 );
