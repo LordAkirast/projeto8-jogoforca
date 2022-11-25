391f4794002b9a5078715b9613aeeb06246edf3b
LordAkirast <darkwarish@gmail.com>
2022-11-24 22:37:24 -0300

    Footer created. Words area created. Project stopped: need to know how to add alphabetic letters properly.

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <div className="game_window">
-    <div className='hangwordzone'>
-      <div className='hangarea' title='hangarea'>
-        <img className="hangman" title='hangman' src="./assets/forca0.png" alt="1 out of 6" />
-      </div>
-      <div className='wordarea'>
-        <button className='button'>Escolher Palavra</button>
+  <>
+    <div className="game_window">
+      <div className='hangwordzone'>
+        <div className='hangarea' title='hangarea'>
+          <img className="hangman" title='hangman' src="./assets/forca0.png" alt="1 out of 6" />
+        </div>
+        <div className='wordarea'>
+          <button className='button'>Escolher Palavra</button>
+        </div>
       </div>
     </div>
-  </div>
+    <footer className='footer'>
+      <div className='words'>
+        <div className='letters'>a</div>
+        <div className='guess'>guess</div>
+      </div>
+
+    </footer>
+  </>
 );
